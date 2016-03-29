6c5841773743bd5d2cd70d2815635455380532d4
h-tsumuraya <hironori-tsumuraya@united.jp>
2016-03-29 15:45:16 +0900

    課題：「Have」「Want」ボタンの実装

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,14 +11,15 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合はAmazonのデータを登録する。
     if @item.new_record?
       begin
-        # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = {}
+        response = Amazon::Ecs.item_lookup(params[:asin],
+                                           :response_group => 'Medium' ,
+                                           :country => 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
 
       amazon_item       = response.items.first
-      @item.title        = amazon_item.get('ItemAttributes/Title')
+      @item.title        = amazon_item.get("ItemAttributes/Title")
       @item.small_image  = amazon_item.get("SmallImage/URL")
       @item.medium_image = amazon_item.get("MediumImage/URL")
       @item.large_image  = amazon_item.get("LargeImage/URL")
@@ -27,19 +28,22 @@ class OwnershipsController < ApplicationController
       @item.save!
     end
 
-    # TODO ユーザにwant or haveを設定する
-    # params[:type]の値にHaveボタンが押された時には「Have」,
-    # Wantボタンが押された時には「Want」が設定されています。
-    
+    if params[:type] == "Have"
+      current_user.have(@item)
+    else
+      current_user.want(@item)
+    end
 
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
+    @item = Item.find(params[:id])
 
-    # TODO 紐付けの解除。 
-    # params[:type]の値にHave itボタンが押された時には「Have」,
-    # Want itボタンが押された時には「Want」が設定されています。
-
+    if params[:type] == "Have"
+      current_user.unhave @item
+    else
+      current_user.unwant @item
+    end
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  
+
   has_many :following_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following_users, through: :following_relationships, source: :followed
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
@@ -15,6 +15,13 @@ class User < ActiveRecord::Base
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
 
+  #Want
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
+
+  #Have
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items, through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -29,22 +36,29 @@ class User < ActiveRecord::Base
     following_users.include?(other_user)
   end
 
-  ## TODO 実装
   def have(item)
+    haves.find_or_create_by(item_id: item.id)
   end
 
   def unhave(item)
+    have_ownership = haves.find_by(item_id: item.id)
+    have_ownership.destroy if have_ownership
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    wants.find_or_create_by(item_id: item.id)
   end
 
   def unwant(item)
+    want_ownership = wants.find_by(item_id: item.id)
+    want_ownership.destroy if want_ownership
   end
 
   def want?(item)
+    want_items.include?(item)
   end
 end
