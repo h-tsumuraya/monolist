7d897565c6984e0908b829df2c74624e0e5e0a7b
h-tsumuraya <hironori-tsumuraya@united.jp>
2016-03-30 15:27:39 +0900

    ランキングページで人数も表示

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,27 +1,29 @@
 class RankingController < ApplicationController
   def have
-    @items = []
+    get_items Have
+  end
 
+  def want
+    get_items Want
+  end
+
+  private
+
+  def get_items(clazz)
+    @items = []
     # item_idでグループ化し、重複数の降順で並び替えて、keyを取得
-    ids = Hash[Have.group(:item_id).count.sort_by { |_, v| -v }].each do |key, value|
+    ids = Hash[clazz.group(:item_id).count.sort_by { |_, v| -v }].each do |key, value|
       # selectを何回も読んでしまっている
-      @items.push Item.find(key)
+      item = Item.find(key)
+      item.rank = value
+      @items.push item
 
       # 10位まで
       if @items.size >= 10
         break
       end
+
     end
   end
 
-  def want
-    @items = []
-    ids = Hash[Want.group(:item_id).count.sort_by { |_, v| -v }].each do |key, value|
-      @items.push Item.find(key)
-      if @items.size >= 10
-        break
-      end
-    end
-
-  end
 end
